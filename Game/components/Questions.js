6ed212e93fb43edd04b968a7fc0fd10e49cb96af
Tamannaaa17 <110052631+Tamannaaa17@users.noreply.github.com>
2023-09-01 19:23:41 +0530

    Add files via upload

---
+++ Game/components/Questions.js
@@ -0,0 +1,24 @@
+// const words = [
+//   { id: 1, word: "Er" },
+//   { id: 8, word: "hungrig" },
+//   { id: 2, word: "isst" },
+//   { id: 7, word: "er" },
+//   { id: 6, word: "weil" },
+//   { id: 9, word: "ist" },
+//   { id: 5, word: "," },
+//   { id: 3, word: "einen" },
+//   { id: 4, word: "Apfel" },
+// ];
+
+const words =[
+    {
+        Questions: 'Sparkle',
+        Options:[
+            { id: 8, word: "Sp" },
+          { id: 5, word: "Schp" },
+          { id: 2, word: "Ƨ9" },
+        { id: 7, word: "er" },],
+        
+    }
+];
+export default words;
